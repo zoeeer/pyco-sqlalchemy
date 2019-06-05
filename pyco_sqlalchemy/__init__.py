b800b52ef4dab37605dd413043a031e6b5d3c590
ningrong <ningrong@minieye.cc>
2019-06-05 22:37:05 +0800

    expose types and flask

---
+++ pyco_sqlalchemy/__init__.py
@@ -0,0 +1,16 @@
+"""
+Tips:
+- use `charset=UTF8MB4 collate utf8_general_ci`
+  (https://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci)
+- use `ext.TrimColumn instead Columns.Text`
+
+# todo: support simple SQLALchemy-ORM
+# todo: support django-ORM
+"""
+
+UTF8 = "utf8_general_ci"
+UTF8MB4 = "utf8mb4_general_ci"
+_UTF8MB4 = "utf8mb4_unicode_ci"
+
+from . import _types as types
+from . import _flask as flask
