31b4e43fcc3b8df94c997c2ce0877ca9ce514ed9
ningrong <ningrong@minieye.cc>
2019-06-14 21:28:04 +0800

    tag v1.0.2 and package

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     name="pyco_sqlalchemy",
     url="https://github.com/dodoru/pyco-sqlalchemy",
     license="MIT",
-    version='1.0.1',
+    version='1.0.2',
     author="dodoru",
     author_email="dodoru@foxmail.com",
     description=(description),
