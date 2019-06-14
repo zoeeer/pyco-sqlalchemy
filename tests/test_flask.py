a639048f50463e35c5ca8f519ccbc4d79b61b01d
ningrong <ningrong@minieye.cc>
2019-06-14 17:23:23 +0800

    test _flask

---
+++ tests/test_flask.py
@@ -0,0 +1,42 @@
+import os
+import pytest
+import tempfile
+from flask import Flask
+from pyco_sqlalchemy._flask import BaseModel, db
+
+cwd = os.path.dirname(__file__)
+
+
+@pytest.fixture
+def app():
+    app = Flask(__name__)
+    # db_file = 'sqlite:///test.sqlite.db'
+    db_fd, db_file = tempfile.mkstemp(suffix="sqlite.db", dir=cwd)
+    print(db_fd, db_file)
+    app.config["TESTING"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///'.format_map(db_file)
+    app.config["SQLALCHEMY_ECHO"] = True
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
+        yield app
+
+    os.close(db_fd)
+    os.unlink(db_file)
+
+
+class User(db.Model, BaseModel):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(32))
+    email = db.Column(db.String(64), unique=True)
+
+
+def test_insert(app):
+    form = dict(name="dev")
+    u1 = User.insert(form, email="dev@pypi.com")
+    u2 = User.upsert_one(form, email="dev@oncode.cc")
+    assert u1.id == u2.id
+    print(u1.to_dict())
+    print(u2.to_dict())
