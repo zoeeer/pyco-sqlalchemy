6e6b2fdc73a168bd5efafa51a45e7f8f8a729c13
dodoru <dodoru@foxmail.com>
2021-04-12 16:32:19 +0800

    release: 1.2.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name="pyco_sqlalchemy",
     url="https://github.com/dodoru/pyco-sqlalchemy",
     license="MIT",
-    version='1.2.0',
+    version='1.2.1',
     author="Nico Ning",
     author_email="dodoru@foxmail.com",
     description=(description),
